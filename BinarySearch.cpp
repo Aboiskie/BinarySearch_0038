c1919339cc2450b996f7f3720b650637867f39df
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-16 15:46:14 +0700

    Add input function to prompt for array size with a maximum limit

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,3 +5,12 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        
